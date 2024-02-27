fe6e53b5529154ad2435a0fdf99d0943474c0015
amandaedunaway <amandaedunaway@gmail.com>
2024-02-27 17:15:42 -0600

    updated schema w/ value lengths

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -16,8 +16,8 @@ DROP TABLE IF EXISTS departments CASCADE;
 
 -- Create new table: 
 CREATE TABLE titles (
-	title_id VARCHAR NOT NULL PRIMARY KEY,
-	title VARCHAR NOT NULL
+	title_id VARCHAR(30) NOT NULL PRIMARY KEY,
+	title VARCHAR(30) NOT NULL
 ); 
 
 -- Verify successful data import: 
@@ -30,11 +30,11 @@ SELECT * FROM titles;
 -- Create new table: 
 CREATE TABLE employees (
 	emp_no INT NOT NULL PRIMARY KEY,
-	emp_title VARCHAR NOT NULL,
+	emp_title VARCHAR(30) NOT NULL,
 	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	sex VARCHAR(30) NOT NULL,
 	hire_date DATE NOT NULL,
 	FOREIGN KEY (emp_title) REFERENCES titles(title_id)
 ); 
@@ -48,8 +48,8 @@ SELECT * FROM employees;
 
 -- Create new table: 
 CREATE TABLE departments (
-	dept_no VARCHAR PRIMARY KEY NOT NULL,
-	dept_name VARCHAR NOT NULL
+	dept_no VARCHAR(30) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(30) NOT NULL
 );
 
 -- Verify successful data import: 
@@ -62,7 +62,7 @@ SELECT * FROM departments;
 -- Create new table: 
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR(30) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	PRIMARY KEY (dept_no, emp_no)
@@ -77,7 +77,7 @@ SELECT * FROM dept_emp;
 
 -- Create new table: 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR(30) NOT NULL,
 	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
